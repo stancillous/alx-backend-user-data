7b077bdaa8ce33f6ae603817e26be14447337ec3
raymond <stancillousray@gmail.com>
2024-01-11 22:53:09 +0300

    create a mysql connection

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,9 @@
 filterd logger solutions
 """
 from typing import List
+import os
 import re
+from mysql.connector import connection
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -13,3 +15,15 @@ def filter_datum(fields: List[str], redaction: str,
         pattern = re.escape(field) + r'=[^' + re.escape(separator) + r']+'
         message = re.sub(pattern, field + '=' + redaction, message)
     return message
+
+
+def get_db():
+    """func that returns a connector to a database"""
+    config = {
+        "user": os.getenv("PERSONAL_DATA_DB_USERNAME"),
+        "host": os.getenv("PERSONAL_DATA_DB_HOST"),
+        "password": os.getenv("PERSONAL_DATA_DB_PASSWORD"),
+        "database": os.getenv("PERSONAL_DATA_DB_NAME")
+    }
+    connection_obj = connection.MySQLConnection(**config)
+    return connection_obj
