c1e791766d2aacf0dc880448652021ad9ce7ee0c
raymond <stancillousray@gmail.com>
2024-01-11 22:59:12 +0300

    create a mysql connection

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-filterd logger solutions
+modue to implement the filterd logger solutions in python
 """
 from typing import List
-from os import environ
+import os
 import re
 from mysql.connector import connection
 
@@ -17,23 +17,13 @@ def filter_datum(fields: List[str], redaction: str,
     return message
 
 
-def get_db():
+def get_db() -> connection.MySQLConnection:
     """func that returns a connector to a database"""
-    # config = {
-    #     "user": os.getenv("PERSONAL_DATA_DB_USERNAME"),
-    #     "host": os.getenv("PERSONAL_DATA_DB_HOST"),
-    #     "password": os.getenv("PERSONAL_DATA_DB_PASSWORD"),
-    #     "database": os.getenv("PERSONAL_DATA_DB_NAME")
-    # }
-    # connection_obj = connection.MySQLConnection(**config)
-    # return connection_obj
-    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = environ.get("PERSONAL_DATA_DB_NAME")
-
-    cnx = connection.MySQLConnection(user=username,
-                                     password=password,
-                                     host=host,
-                                     database=db_name)
-    return cnx
+    config = {
+        "user": os.getenv("PERSONAL_DATA_DB_USERNAME"),
+        "host": os.getenv("PERSONAL_DATA_DB_HOST"),
+        "password": os.getenv("PERSONAL_DATA_DB_PASSWORD"),
+        "database": os.getenv("PERSONAL_DATA_DB_NAME")
+    }
+    connection_obj = connection.MySQLConnection(**config)
+    return connection_obj
