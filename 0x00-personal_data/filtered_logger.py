774e95b9231dd1c4892cdc39abab1cd688079c79
raymond <stancillousray@gmail.com>
2024-01-11 22:33:13 +0300

    use regex to replace certain fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,13 +2,13 @@
 """
 filterd logger solutions
 """
-import re
 from typing import List
+import re
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: List[str], separator: str) -> List:
-    """obfuscates a message and returns it"""
+    """function that obfuscates a message and returns it"""
     for field in fields:
         pattern = re.escape(field) + r'=[^' + re.escape(separator) + r']+'
         message = re.sub(pattern, field + '=' + redaction, message)
