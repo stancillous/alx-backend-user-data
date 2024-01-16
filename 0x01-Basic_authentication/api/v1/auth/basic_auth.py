0997a4a4192359a6b4be3bf1538476b9f816f9d3
raymond <stancillousray@gmail.com>
2024-01-16 15:06:27 +0300

    add method to decode b64encoded string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,10 +3,10 @@
 BasicAuth class
 """
 import base64
-from api.v1.auth.auth import Auth
+# from api.v1.auth.auth import Auth
 
 
-class BasicAuth(Auth):
+class BasicAuth():
     """BasicAuth class"""
     def __init__(self):
         """constructor"""
@@ -20,3 +20,17 @@ class BasicAuth(Auth):
                 not authorization_header.startswith("Basic ")):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """decodes base64 auth header"""
+        if ((base64_authorization_header is None) or
+                (type(base64_authorization_header) != str)):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode()
+            return decoded_str
+        except Exception:
+            return None
