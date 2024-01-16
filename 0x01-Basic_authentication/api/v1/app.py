718deed6a8c238f880a96bd27aa9b2152a592afe
raymond <stancillousray@gmail.com>
2024-01-16 14:10:01 +0300

    add a before_request handler

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,27 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+auth = os.getenv("AUTH_TYPE")
+
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """func to run before each request"""
+
+    if auth is not None:
+        excluded_paths = ['/api/v1/status/',
+                          '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
