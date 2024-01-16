afd7e80139c6c9855d6de4db1313d95cdbe1d928
raymond <stancillousray@gmail.com>
2024-01-16 12:13:14 +0300

    add test function for 401 error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,6 +14,11 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route("/unauth")
+def unauth():
+    """testing the 401 error"""
+    abort(401)
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
