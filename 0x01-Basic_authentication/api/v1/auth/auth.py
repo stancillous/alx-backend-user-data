e068cc0c4f6acbbc918c511f9cfdba8c80a59f20
raymond <stancillousray@gmail.com>
2024-01-16 14:10:53 +0300

    update methods in the Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -27,7 +27,10 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """auth header method
         @request: Flask request object"""
-        return None
+
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current user method
