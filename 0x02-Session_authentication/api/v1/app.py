4380b04d728ff41c54fba7b312703e53363d7e60
raymond <stancillousray@gmail.com>
2024-01-18 11:01:37 +0300

    update before_request decorator

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,10 +39,10 @@ def before_request():
                           '/api/v1/unauthorized/', '/api/v1/forbidden/',
                           '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, excluded_paths):
-            if auth.current_user(request) is None:
-                abort(403)
             if auth.authorization_header(request) is None:
                 abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
             if (auth.authorization_header(request) is None
                     and auth.session_cookie(request) is None):
                 abort(401)
