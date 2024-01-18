f6c4201cf09da4c8fcfbc91bbcc8718a44447192
raymond <stancillousray@gmail.com>
2024-01-18 10:27:38 +0300

    add users to session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,21 @@
 SessionAuth class
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class"""
+    user_id_by_session_id = {}
+
     def __init__(self):
         """constructor"""
         pass
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates session id for a user_id"""
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
