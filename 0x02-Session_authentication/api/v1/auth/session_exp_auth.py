6c89e62de3898cd3a07c2e800aef3de479c4f884
raymond <stancillousray@gmail.com>
2024-01-18 17:18:32 +0300

    fix pycodestyle errors

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""class SessionExpAuth that will add an 
+"""class SessionExpAuth that will add an
 expiration date to the session id"""
 from api.v1.auth.session_auth import SessionAuth
 import os
@@ -25,20 +25,20 @@ class SessionExpAuth(SessionAuth):
             "user_id": user_id,
             "created_at": datetime.now()
         }
-        self.user_id_by_session_id[session_id] 
+        self.user_id_by_session_id[session_id] = session_dictionary
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """overloads the method in the parent class"""
-        if session_id is None or self.user_id_by_session_id.get(session_id) is None:
+        if (session_id is None or
+                self.user_id_by_session_id.get(session_id) is None):
             return None
         user_id = super().user_id_for_session_id(session_id)
         if self.session_duration <= 0:
-            print("\n\n\t\tuser id ", self.user_id_by_session_id.get(session_id))
             return self.user_id_by_session_id.get(session_id)
         if self.user_id_by_session_id.get("created_at") is None:
             return None
-        
+
         created_at = self.user_id_by_session_id.get("created_at")
         time_period = created_at + timedelta(seconds=self.session_duration)
 
