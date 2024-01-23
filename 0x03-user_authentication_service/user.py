57b6181a00df36e8ad5946fc73b07d96f89e5873
raymond <stancillousray@gmail.com>
2024-01-23 12:38:22 +0300

    user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,3 +15,8 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __init__(self, email, hashed_password):
+        """constructor method"""
+        self.email = email
+        self.hashed_password = hashed_password
