50cee071750050e845302b3b44863d91c5e5b7cf
raymond <stancillousray@gmail.com>
2024-01-24 14:19:09 +0300

    add long docstring

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,7 +2,7 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
@@ -22,7 +22,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -37,65 +37,9 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Creates and adds a new user to the database.
-
-        Args:
-            email (str): The email of the new user.
-            hashed_password (str): The hashed password of the new user.
-
-        Returns:
-            User: The newly created user object.
+        """method to add a user
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
-
-    def find_user_by(self, **kwargs) -> User:
-        """
-        Find a user by the provided keyword arguments.
-
-        Args:
-            **kwargs (dict): Arbitrary keyword arguments.
-
-        Returns:
-            User: The first user found based on the provided keyword arguments.
-
-        Raises:
-            NoResultFound: If no user is found.
-            InvalidRequestError: If the request is invalid.
-        """
-        if not kwargs:
-            raise InvalidRequestError()
-        for keyword, value in kwargs.items():
-            if hasattr(User, keyword):
-                first_user = self._session.query(User).filter_by(
-                    **{keyword: value}).first()
-                if first_user is not None:
-                    return first_user
-                else:
-                    raise NoResultFound()
-            else:
-                raise InvalidRequestError()
-
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """
-        Updates a user with the specified user
-        ID using the provided keyword arguments.
-
-        Args:
-            user_id (int): The ID of the user to update.
-            **kwargs: Arbitrary keyword arguments to update the user with.
-
-        Returns:
-            None
-        """
-        user = self.find_user_by(id=user_id)
-
-        for keyword, value in kwargs.items():
-            if hasattr(User, keyword):
-                setattr(user, keyword, value)
-            else:
-                raise ValueError()
-        self._session.commit()
