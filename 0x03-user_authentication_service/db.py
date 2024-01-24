cdfbac6052c2f5cd34b68e85547e4da8be26544f
raymond <stancillousray@gmail.com>
2024-01-24 11:57:47 +0300

    change module

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,68 +1,158 @@
 #!/usr/bin/env python3
-"""DB module
+"""_summary_
 """
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
+
+
+import bcrypt
+from db import DB
+from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
-from user import Base, User
-from typing import TypeVar
+from typing import Union
 
 
-class DB:
-    """DB class
+def _hash_password(password: str) -> str:
+    """_summary_
+
+    Args:
+        password (str): _description_
+
+    Returns:
+        bytes: _description_
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """_summary_
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        str: _description_
+    """
+    id = uuid4()
+    return str(id)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
     """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
+    def __init__(self):
+        """_summary_
         """
-        self._engine = create_engine("mysql://ray:raypassword@localhost/alxdb",
-                                     echo=False)
-        Base.metadata.drop_all(self._engine)
-        Base.metadata.create_all(self._engine)
-        self.__session = None
+        self._db = DB()
 
-    @property
-    def _session(self) -> Session:
-        """Memoized session object
+    def register_user(self, email: str, password: str) -> Union[None, User]:
+        """_summary_
         """
-        if self.__session is None:
-            DBSession = sessionmaker(bind=self._engine)
-            self.__session = DBSession()
-        return self.__session
-
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """method to add a user"""
-        new_user = User(email=email, hashed_password=hashed_password)
-        session = self._session
-        session.add(new_user)
-        session.commit()
-        return new_user
-
-    def find_user_by(self, **kwargs) -> User:
-        """get the first row found in users table"""
-        if not kwargs:
-            raise InvalidRequestError
-
-        session = self._session
-        our_user = session.query(User).filter_by(**kwargs).first()
-        if our_user is None:
-            raise NoResultFound
-        return our_user
-
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """update user details in db"""
-        session = self._session
         try:
-            user = self.find_user_by(id=user_id)
-            for key, value in kwargs.items():
-                if key in user.__dict__:
-                    setattr(user, key, value)
-                else:
-                    raise ValueError
-            session.commit()
-        except Exception:
+            # find the user with the given email
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            # add user to database
+            return self._db.add_user(email, _hash_password(password))
+
+        else:
+            # if user already exists, throw error
+            raise ValueError('User {} already exists'.format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """_summary_
+
+        Args:
+            email (str): _description_
+            password (str): _description_
+
+        Returns:
+            Boolean: _description_
+        """
+        try:
+            # find the user with the given email
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        # check validity of password
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """_summary_
+
+        Args:
+            email (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = _generate_uuid()
+            return user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """_summary_
+
+        Args:
+            session_id (_type_): _description_
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
+
+    def destroy_session(self, user_id: str) -> None:
+        """_summary_
+
+        Args:
+            user_id (str): _description_
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = None
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """_summary_
+
+        Args:
+            email (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
             raise ValueError
+        else:
+            user.reset_token = _generate_uuid()
+            return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """_summary_
+
+        Args:
+            reset_token (str): _description_
+            password (str): _description_
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+            return None
